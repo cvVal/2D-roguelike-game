fb63652c5c5860154b9265faaeba7db2774b9694
cvVal <cvvaldez_00@hotmail.com>
2025-07-26 17:50:51 -0400

    feat: Add animation to the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,10 +10,14 @@ public class PlayerController : MonoBehaviour
     private BoardManager m_BoardManager;
     private Vector2Int m_CellPosition;
     private bool m_IsGameOver;
+    private Animator m_Animator;
+    private bool m_IsAttacking;
 
     public void Init()
     {
         m_IsGameOver = false;
+        m_IsAttacking = false;
+        m_Animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,6 +31,9 @@ public class PlayerController : MonoBehaviour
 
             return;
         }
+
+        // Don't accept input during attack animation
+        if (m_IsAttacking) return;
 
         Vector2Int newCellTarget = m_CellPosition;
         bool hasMoved = false;
@@ -62,10 +70,24 @@ public class PlayerController : MonoBehaviour
                 {
                     MoveTo(newCellTarget);
                 }
-                else if (cellData.ContainedObject.PlayerWantsToEnter())
+                else
                 {
-                    MoveTo(newCellTarget);
-                    cellData.ContainedObject.PlayerEntered();
+                    // Check if this is an attackable object (wall, enemy, etc.)
+                    bool isAttackable = cellData.ContainedObject is WallObject;
+                    
+                    if (isAttackable)
+                    {
+                        StartCoroutine(AttackSequence(cellData.ContainedObject, newCellTarget));
+                    }
+                    else
+                    {
+                        // Non-attackable objects (like food) - no animation, just interact
+                        if (cellData.ContainedObject.PlayerWantsToEnter())
+                        {
+                            MoveTo(newCellTarget);
+                            cellData.ContainedObject.PlayerEntered();
+                        }
+                    }
                 }
             }
         }
@@ -92,5 +114,33 @@ public class PlayerController : MonoBehaviour
     public void GameOver()
     {
         m_IsGameOver = true;
+    }
+
+    /// <summary>
+    /// Handles attack sequence with proper timing to prevent visual glitches.
+    /// </summary>
+    private IEnumerator AttackSequence(CellObject target, Vector2Int targetCell)
+    {
+        m_IsAttacking = true;
+
+        if (m_Animator != null)
+        {
+            m_Animator.SetTrigger("Attack");
+        }
+
+        // Wait for a brief moment to let animation start
+        yield return new WaitForSeconds(0.3f);
+
+        if (target.PlayerWantsToEnter())
+        {
+            // Target was destroyed/allows entry, player can move
+            MoveTo(targetCell);
+            target.PlayerEntered();
+        }
+
+        // Wait for attack animation to finish
+        yield return new WaitForSeconds(0.2f);
+
+        m_IsAttacking = false;
     }
 }
